c10fd0de92a345c7e20676af9c10a99209861879
Kawaharasouta <kawahara6514@gmail.com>
2018-06-07 19:26:09 +0900

    save

--- src/pkt_io.c
+++ src/pkt_io.c
@@ -14,12 +14,13 @@
 struct queue_info{
 	int num;
 	struct pkt_queue *head;
-}
+	struct pkt_queue *tail;
+};
 struct pkt_queue {
-	struct *rte_mbuf mbuf;	//uint8_t?
-	int size;
-	struct *pkt_queue next;
-}
+	struct rte_mbuf *mbuf;	//uint8_t?
+	size_t size;
+	struct pkt_queue *next;
+};
 
 struct queue_info tx_queue;
 struct queue_info rx_queue;
@@ -31,15 +32,64 @@ int queue_init() {
 	rx_queue.head = NULL;
 }
 
-int queue_push() {
-	
+void tx_queue_push(struct rte_mbuf *mbuf) {
+	tx_queue.num += 1;
+	struct pkt_queue *pkt = (struct pkt_queue *)malloc(sizeof(struct pkt_queue));
+	pkt->mbuf = mbuf;
+	pkt->size = rte_pktmbuf_pkt_len(mbuf);
+	if (tx_queue.head == NULL){
+		tx_queue.head = pkt;
+		tx_queue.tail = pkt;
+	}
+
+	tail->next = pkt;
+	tail = pkt;
 }
 
-int queue_pop
+struct rte_mbuf* tx_queue_pop() {
+	if (tx_queue.head == NULL) {
+		return NULL;
+	}
+	tx_queue.num -= 1;
+
+	struct rte_mbuf *ret = tx_queue.head->mbuf;
+	struct pkt_queue *dust = tx_queue.head;
+	tx_queue.head = tx_queue.head->next;
+	free(dust);
+	//if head is NULL, tail can be anything
+	return ret;
+}
+
+void rx_queue_push(struct rte_buf *mbuf) {
+	rx_queue.num += 1;
+	struct pkt_queue *pkt = (struct pkt_queue *)malloc(sizeof(struct pkt_queue));
+	pkt->mbuf = mbuf;
+	if (tx_queue.head == NULL){
+		rx_queue.head = pkt;
+		rx_queue.tail = pkt;
+	}
+
+	tail->next = pkt;
+	tail = pkt;
+}
+
+struct rte_mbuf* rx_queue_pop() {
+	if (rx_queue.head == NULL) {
+		return NULL;
+	}
+	rx_queue.num -= 1;
+
+	struct rte_mbuf *ret = rx_queue.head->mbuf;
+	struct pkt_queue *dust = rx_queue.head;
+	rx_queue.head = rx_queue.head->next;
+	free(dust);
+	//if head is NULL, tail can be anything
+	return ret;
+}
 
 
 struct port {
-  uint8_t port;
+  uint8_t port_num;
 }
 
 struct rte_mempool *mbuf_pool;
@@ -57,7 +107,7 @@ static int
 port_init(uint16_t port)
 { 
 	struct rte_eth_conf port_conf = port_conf_default;
-	const uint16_t rx_rings = 128, tx_rings = 512;
+	const uint16_t rx_rings = 1, tx_rings = 1;
 	uint16_t nb_rxd = RX_RING_SIZE;
 	uint16_t nb_txd = TX_RING_SIZE;
 	int retval;
@@ -153,9 +203,9 @@ port
 		perror("malloc");
 		return NULL;
 	}
-	port->port = (uint16_t)atoi(name);
+	port->port_num = (uint16_t)atoi(name);
 	
-	if (port_init(port->port) < 0){
+	if (port_init(port->port_num) < 0){
 		free(port);
 		return NULL;
 	}
@@ -165,51 +215,65 @@ port
 
 void 
 port_close (port *port) {
-	printf("close port %u\n", port->port);
-	rte_eth_dev_stop(port->port);
-	rte_eth_dev_close(port->port);
+	printf("close port %u\n", port->port_num);
+	rte_eth_dev_stop(port->port_num);
+	rte_eth_dev_close(port->port_num);
 	free(port);
 }
 
 void
-rx_pkt (port *port, void (*callback)(uint8_t *, size_t), int timeout) {
+rx_pkt (port *port) {
 	uint16_t nb_ports;
-	uint16_t port = port->port;
+	uint16_t nport = port->port_num;
 	struct rte_mbuf *bufs[BURST_SIZE];
 	uint16_t nb_rx;
 	nb_ports = rte_eth_dev_count();
 
 	/* Recv burst of RX packets */
-	nb_rx = rte_eth_rx_burst(port, 0, bufs, BURST_SIZE);
+	nb_rx = rte_eth_rx_burst(nport, 0, bufs, BURST_SIZE);
 	int i;
 	for (i = 0; i < nb_rx ; i++) {
 		uint8_t *p = rte_pktmbuf_mtod(bufs[i], uint8_t*);
-		size_t size = rte_pktmbuf_pkt_len(bufs[i]);
+		//size_t size = rte_pktmbuf_pkt_len(bufs[i]);
   
-		callback(p, size);
+		tx_queue_push(bufs[i]);
 	}
 }
 
-ssize_t
-tx_pkt (port *port, const uint8_t *buffer, size_t length) {
+size_t
+tx_pkt (port *port) {
 	struct rte_mbuf *bufs[BURST_SIZE];
 	uint16_t nb_ports;
-	uint16_t port = port->port;
+	uint16_t nport = port->port_num;
 	uint8_t *p;
 	nb_ports = rte_eth_dev_count();
-  
+ 
+  int i;
+	for (i = 0; i < BURST_SIZE; i++){
+		bufs[i] = tx_queue_pop();
+		if (bufs[i] == NULL){
+			break;
+		}
+	}
 	/* Send burst of TX packets */
-	bufs[0] = rte_pktmbuf_alloc(mbuf_pool);
-	bufs[0]->pkt_len = length;
-	bufs[0]->data_len = length;
-	bufs[0]->port = port;
-	bufs[0]->packet_type = 1;
+	//bufs[0] = rte_pktmbuf_alloc(mbuf_pool);
+	//bufs[0]->pkt_len = length;
+	//bufs[0]->data_len = length;
+	//bufs[0]->port = nport;
+	//bufs[0]->packet_type = 1;
+  //
+	//p = rte_pktmbuf_mtod(bufs[0], uint8_t*);
+	//memcpy(p, buffer, length);
   
-	p = rte_pktmbuf_mtod(bufs[0], uint8_t*);
-	memcpy(p, buffer, length);
-  
-	if (rte_eth_tx_burst(port, 0, bufs, 1) > 0) {
-		return length;
+	size_t num_tx;
+	num_tx = rte_eth_tx_burst(nport, 0, bufs, 1);
+	if (num_tx < i){
+		for (uint16_t j = 0; j < i - num_tx; j++){
+			rte_pktmbuf_free(bufs[num_tx + j]);
+		}
+	}
+	if (num_pkt > 0) {
+		return num_tx;
 	}
 	return -1;
 }
