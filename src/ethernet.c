bc05cf64ff8fe9d7d7e649820f0cf4ec51070ac2
Kawaharasouta <kawahara6514@gmail.com>
2018-06-19 15:49:38 +0900

    save

--- src/ethernet.c
+++ src/ethernet.c
@@ -2,12 +2,15 @@
 #include<stdlib.h>
 #include<string.h>
 
+#include <rte_mbuf.h>
 
 
-size_t tx_ether(){
-			
+
+size_t tx_ether(/*struct rte_mbuf *mbuf, uint32_t size*/void){
+	struct queue_pkt queue_pkt = rx_queue_pop()
+	uint8_t *p = rte_pktmbuf_mtod(mbuf, uint8_t*);
 }
 
-size_t rx_ether(){
+size_t rx_ether(struct rte_mbuf *mbuf, uint32_t size){
 
 }
