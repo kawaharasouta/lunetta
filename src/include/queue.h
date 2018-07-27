88db729e670353696ddd8dcf5d1730785ff2d564
Kawaharasouta <kawahara6514@gmail.com>
2018-07-28 02:33:03 +0900

    queue branch init

---
+++ src/include/queue.h
@@ -0,0 +1,5 @@
+#ifndef __QUEUE_H_
+#define __QUEUE_H_
+
+
+#endif
---
+++ src/queue.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdint.h>
+
+#include"include/queue.h"
+
+int main(int argc, char **argv){
+
+}
