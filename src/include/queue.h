3a162e54b8975866d0efe0631eaa20c3a7f4f69a
Kawaharasouta <kawahara6514@gmail.com>
2018-07-28 16:02:59 +0900

    save

--- src/include/queue.h
+++ src/include/queue.h
@@ -1,5 +1,28 @@
 #ifndef __QUEUE_H_
 #define __QUEUE_H_
 
+#include<pthread.h>
+
+#define QUEUE_SIZE 10000
+
+struct allocate_pool {
+	uint16_t num;
+	struct queue_node *head;
+	struct queue_node *tail;
+	struct queue_node *next;
+} allocate_pool;
+
+struct queue_info{
+	int num;
+	struct queue_node *head;
+	struct queue_node *tail;
+	struct allocate_pool pool;
+	//pthread_mutex_t mutex;
+};
+struct queue_node {
+	void *data;
+	uint32_t size;
+	struct queue_node *next;
+};
 
 #endif
--- src/queue.c
+++ src/queue.c
@@ -5,6 +5,84 @@
 
 #include"include/queue.h"
 
-int main(int argc, char **argv){
-
+int allocate_pool_brk(struct queue_info *queue) {
+  queue->pool.head = (struct queue_node *)malloc(sizeof(struct queue_node) * QUEUE_SIZE);
+	if (!queue->pool.head)	return 0;
+  queue->pool.next = allocate_pool.head;
+  queue->pool.tail = allocate_pool.head + QUEUE_SIZE - 1;
+  queue->pool.num = QUEUE_SIZE;
+	return 1;
 }
+
+struct queue_node* allocate_queue_node(struct queue_info *queue) {
+  struct queue_node *ret = queue->pool.next;
+
+  if (queue->pool.next == queue->pool.tail)
+    queue->pool.next = queue->pool.head;
+  else
+    queue->pool.next++;
+
+  return ret;
+}
+
+int queue_init(struct queue_info *queue) {
+  queue->head = NULL;
+  queue->tail = NULL;
+  queue->num = 0;
+  //pthread_mutex_init(&queue->mutex, NULL);
+
+  //rx_queue.head = NULL;
+  //rx_queue.tail = NULL;
+  //rx_queue.num = 0;
+  //pthread_mutex_init(&rx_queue.mutex, NULL);
+
+  return allocate_pool_brk(queue);
+}
+
+void queue_push(struct queue_info *queue, void *data, uint32_t size) {
+	if (data == NULL)
+    return;
+
+  //pthread_mutex_lock(&tx_queue.mutex);
+  queue->num += 1;
+  //struct pkt_queue *pkt = (struct pkt_queue *)malloc(sizeof(struct pkt_queue));
+  struct queue_node *node = allocate_queue_node(queue);
+  node->data = data;
+  //!it is really wrong
+  node->size = size;
+  node->next = NULL;
+#if 1
+  if (!tx_queue.head){
+    tx_queue.head = pkt;
+    tx_queue.tail = pkt;
+    return;
+  }
+  tx_queue.tail->next = pkt;
+  tx_queue.tail = pkt;
+#else
+  if (rx_queue.tail) {
+    rx_queue.tail->next = pkt;
+  }
+  rx_queue.tail = pkt;
+  if (!rx_queue.head){
+    rx_queue.head = pkt;
+  }
+#endif
+
+  //pthread_mutex_unlock(&tx_queue.mutex);
+  return;
+}
+
+
+
+
+#ifdef QUEUE_DEBUG
+
+int main(void) {
+	struct queue_info queue;
+
+	if (!queue_init(&queue))
+		printf("queue_init error no tumori ha-to\n");
+}
+
+#endif
